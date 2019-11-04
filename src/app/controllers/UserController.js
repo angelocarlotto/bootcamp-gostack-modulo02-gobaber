b36a09cd57b0c0b8c869dfca763cb0a530235473
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2019-11-04 15:29:30 -0400

    criado no crontoller de usuario o metodo de cadastro de usuários

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,16 @@
+import User from '../models/User';
+
+class UserControler {
+    async store(req, res) {
+        const userExist = await User.findOne({
+            where: { email: req.body.email },
+        });
+        if (userExist) {
+            return res.status(400).json({ erro: 'user already exist' });
+        }
+        const { id, name, email, provider } = await User.create(req.body);
+        return res.json({ id, name, email, provider });
+    }
+}
+
+export default new UserControler();
