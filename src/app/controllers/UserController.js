a10e1b7d406e86a63cc1d3b25bf9266ac4b58021
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2019-11-04 16:46:05 -0400

    criado update do usuário: nome, email e senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,7 +13,24 @@ class UserControler {
     }
 
     async update(req, res) {
-        return res.json({ ok: req.userId });
+        const { email, oldPassword } = req.body;
+        const user = await User.findByPk(req.userId);
+        if (user.email !== email) {
+            const userExist = await User.findOne({
+                where: { email },
+            });
+            if (userExist) {
+                return res.status(400).json({ erro: 'user already exist' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ erro: 'password does not match' });
+        }
+
+        const { id, name, provider } = await user.update(req.body);
+
+        return res.json({ id, name, email, provider });
     }
 }
 
