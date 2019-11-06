8595703e7cb2d0e8f87d7a06771e1fed651917be
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2019-11-06 09:30:44 -0400

    incluido o pacote nodemailer, criado as configuracoes, criado conta no mailtrap para testes, e altero metodo de cancelamento apra inclusive enviar email quando um appointment for cancelado

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    secure: false,
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+        user: 'd40e43f32b06e6',
+        pass: 'd81171023285f8',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarer.com>',
+    },
+};
