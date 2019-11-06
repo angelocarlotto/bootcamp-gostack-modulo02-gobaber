a974083e341a29aa35bd462e09d949f910c33178
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2019-11-05 22:21:14 -0400

    criado controller e metodo para armazenamento de appointments

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleWares from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -17,4 +18,5 @@ routes.use(authMiddleWares);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
 export default routes;
