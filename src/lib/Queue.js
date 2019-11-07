78fac117a32dd5eee930d8bd90f6c394e2f520eb
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2019-11-07 08:56:07 -0400

    criado tratamento de erro durante o processamento ddas filas

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -28,8 +28,12 @@ class Queue {
     processQueue() {
         jobs.forEach(job => {
             const { bee, handle } = this.queues[job.key];
-            bee.process(handle);
+            bee.on('failed', this.handleFailer).process(handle);
         });
+    }
+
+    handleFailer(job, err) {
+        console.log(`Queue ${job.queue.name}: FAILED`, err);
     }
 }
 
