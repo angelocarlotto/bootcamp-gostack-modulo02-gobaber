a318ad6e36344a83c50a47549d96c680113dbf0d
Angelo Carlotto <angelocarlotto@Angelos-MacBook-Pro.local>
2019-11-07 09:30:44 -0400

    listagem de todos os horarios disponiveis para determinado provider

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
 import AppointmentController from './app/controllers/AppointmentController';
+import AvaiableController from './app/controllers/AvaiableController';
 import authMiddleWares from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -18,7 +19,10 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleWares);
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/avaiable', AvaiableController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
@@ -26,6 +30,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedules', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
